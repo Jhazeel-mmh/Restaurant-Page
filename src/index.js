e12d7c917b28b5fc769f6f567bad6150f9b1ed6e
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-09-20 15:56:53 -0600

    Add the setNavItemActive() to changue dinamically the styles of the nav items also add event listeners to the nav and mainBtnToMenu with some validation

--- src/index.js
+++ src/index.js
@@ -3,4 +3,28 @@ import "./style.css";
 import { pagueLoadMainContent } from "./page-load-main.js";
 
 pagueLoadMainContent();
-console.log("Bienvenidos a la hormiga");
+console.log("Bienvenidos al cafe");
+
+const $ = (el) => document.querySelector(el);
+const $$ = (el) => document.querySelectorAll(el);
+
+let nav = document.querySelector("nav");
+let mainBtnToMenu = $(".mainbtn");
+
+function setNavItemActive(e, fixedId = ""){
+    let navItems = $$(".nav-item");
+    navItems.forEach(btn => btn.classList.remove("nav-item-selected"));
+    let navItemId;
+
+    (!e) ? navItemId = fixedId : navItemId = e.target.id; 
+
+    $(`#${navItemId}`).classList.add("nav-item-selected");
+}
+
+nav.addEventListener("click", e => {
+    if (!e.target.classList.contains("nav-item")) return;
+    setNavItemActive(e);
+});
+
+mainBtnToMenu.addEventListener("click", () => setNavItemActive(null, "menu"));
+
